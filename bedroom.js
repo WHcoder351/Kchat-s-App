c28d11252511bea8253c7fd0dcbac1e4e064275f
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-08-22 14:11:03 -0400

    Add files via upload

--- bedroom.js
+++ bedroom.js
@@ -1,5 +1,6 @@
 img = "";
 stats = "";
+Re = [];
 function preload(){
     img = loadImage("BedRoom.jpg");
 }
@@ -13,7 +14,6 @@ function setup() {
 function ml(){
     console.log("CoCoSSD model Loaded!");
     stats = true;
-    obd.detect(img, gR);
 }
 
 function gR(error, results){
@@ -21,8 +21,23 @@ function gR(error, results){
         console.log(error);
     } else {
         console.log(results);
+        Re = results;
     }
 }
 function draw() {
     image(img,0,0,450,450);
+
+    if (stats != "") {
+    obd.detect(img, gR);
+    for(i = 0; i<Re.length; i++){
+        document.getElementById("stats").innerHTML = "Status = Object detected";
+        document.getElementById("OBDs").innerHTML = "# of Objects detected: "+Re.length;
+        percent = floor(Re[i].confidence * 100);
+        fill("blue");
+    text(Re[i].label + " " +percent + "%", Re[i].x +15, Re[i].y+15);
+    noFill();
+    stroke("black");
+    rect(Re[i].x, Re[i].y, Re[i].width, Re[i].height);
+    }
+    }
 }
